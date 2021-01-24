af512a92d6ce2f87720d49f33397bcced24241a7
minnyww <minrrverse@gmail.com>
2021-01-24 17:55:26 +0700

    [EDIT] to image

--- pages/index.js
+++ pages/index.js
@@ -4,32 +4,34 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home({ blogItems }) {
-   console.log(blogItems);
    return (
       <Layout>
          <div className="entries">
             <div className="row justify-content-start  ">
-               {blogItems.map((item) => (
-                  <div className="col-md-6" key={item?.id}>
-                     <div className="entry mb-3">
-                        <Link as={`/blogs/${item.slug}`} href="/blogs/[id]">
-                           <div className="main-image">
-                              <Image
-                                 src={
-                                    process.env.NODE_ENV === "production"
-                                       ? `https://strapi-x39d.onrender.com/uploads/${item?.image?.name}`
-                                       : item?.image?.name
-                                 }
-                                 width={600}
-                                 height={400}
-                                 alt={item?.Headline}
-                              />
-                              <h3>{item?.Headline}</h3>
-                           </div>
-                        </Link>
+               {blogItems.map((item) => {
+                  return (
+                     <div className="col-md-6" key={item?.id}>
+                        <div className="entry mb-3">
+                           <Link as={`/blogs/${item.slug}`} href="/blogs/[id]">
+                              <div className="main-image">
+                                 <Image
+                                    src={
+                                       // process.env.NODE_ENV === "production"
+                                       //    ? `https://strapi-x39d.onrender.com/uploads/${item?.image?.name}`
+                                       //    :
+                                       item?.image?.name
+                                    }
+                                    width={600}
+                                    height={400}
+                                    alt={item?.Headline}
+                                 />
+                                 <h3>{item?.Headline}</h3>
+                              </div>
+                           </Link>
+                        </div>
                      </div>
-                  </div>
-               ))}
+                  );
+               })}
             </div>
          </div>
       </Layout>
