c10b5a4c75312210e74f94e1497ff7c353801236
minnyww <minrrverse@gmail.com>
2021-01-24 17:28:56 +0700

    🎉 [edit] update

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,19 +1,14 @@
 import Layout from "../components/Layout";
 import fetchFromCMS from "../lib/services";
 import Image from "next/image";
-import processMarkdown from "../lib/processMarkdown";
 import ReactMarkdown from "react-markdown";
 const BlogsItem = ({ blogs }) => {
-   console.log(
-      "blogs : ",
-      processMarkdown(blogs.content).then((content) => content),
-   );
    return (
       <Layout>
          <div className="row">
             <div className="portfolio-image text-center mb-4">
                <div className="col-md-12">
-                  <Image src={blogs.image.name} width={1000} height={500} />
+                  <Image src={blogs?.image?.name} width={1000} height={500} />
                </div>
             </div>
          </div>
@@ -21,7 +16,7 @@ const BlogsItem = ({ blogs }) => {
             <div className="portfolio-content">
                <div className="col-md-12">
                   <div className="portfolio-headline text-center m-2">
-                     <h1>{blogs.Headline}</h1>
+                     <h1>{blogs?.Headline}</h1>
                   </div>
                   <ReactMarkdown source={blogs.content} escapeHtml={false} />
                </div>
--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,17 @@ export default function Home({ blogItems }) {
          <div className="entries">
             <div className="row justify-content-start  ">
                {blogItems.map((item) => (
-                  <div className="col-md-6" key={item.id}>
+                  <div className="col-md-6" key={item?.id}>
                      <div className="entry mb-3">
                         <Link as={`/blogs/${item.slug}`} href="/blogs/[id]">
                            <div className="main-image">
                               <Image
-                                 src={item.image.name}
+                                 src={item?.image?.name}
                                  width={600}
                                  height={400}
-                                 alt={item.Headline}
+                                 alt={item?.Headline}
                               />
-                              <h3>{item.Headline}></h3>
+                              <h3>{item?.Headline}></h3>
                            </div>
                         </Link>
                      </div>
