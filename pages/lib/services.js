1c32e2416b0031818c6426ec13fc0448445ab710
minnyww <minrrverse@gmail.com>
2021-01-24 17:24:50 +0700

    [edit] endpoint

--- pages/lib/services.js
+++ pages/lib/services.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 const fetchFromCMS = async (path) => {
-   const url = `http://localhost:5000/${path}`;
+   const url = `${
+      process.env.NODE_ENV === "production"
+         ? `https://strapi-x39d.onrender.com/${path}`
+         : `http://localhost:1337/${path}`
+   } `;
    const res = await axios.get(url);
    return res.data;
 };
