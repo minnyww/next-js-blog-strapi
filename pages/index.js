ef19fce518aa37c8d8058ff2710f8b83e75ae473
minnyww <minrrverse@gmail.com>
2021-01-27 01:24:53 +0700

    [EDIT] new blog style

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Layout from "../components/Layout";
 export default function Home() {
    return (
       <Layout>
+         {/* <Head></Head> */}
          <div className="container-sm">
             <div style={{ margin: "18px" }}>
-               <h1>I'm Min</h1>
+               <h4>Hi there, I'm Apisit Amnuayworrabut</h4>
+               <p>I’m currently working on Edtech Startup - Vonder Thailand</p>
             </div>
          </div>
       </Layout>
